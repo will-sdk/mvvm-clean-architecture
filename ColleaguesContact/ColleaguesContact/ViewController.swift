af653d6db91b45852b839388c1b2b618d0324249
Willy-SDK <>
2022-10-14 23:10:48 +0100

    -init project

---
+++ ColleaguesContact/ColleaguesContact/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColleaguesContact
+//
+//  Created by Willy on 14/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
