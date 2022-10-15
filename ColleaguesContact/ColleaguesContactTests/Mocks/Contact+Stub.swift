626e86ec49f48e4d7262be0635aa843aa3a5230d
Willy-SDK <>
2022-10-15 14:35:04 +0100

    -Mocks stub

--- ColleaguesContact/ColleaguesContactTests/Mocks/Contact+Stub.swift
+++ ColleaguesContact/ColleaguesContactTests/Mocks/Contact+Stub.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+extension Contact {
+    static func stub(id: Contact.Identifier = "1",
+                     firstName: String = "Maggie" ,
+                     lastName: String = "Brekke",
+                     email: String? = "Crystel.Nicolas61@hotmail.com",
+                     jobtitle: String = "Future Functionality Strategist",
+                     favouriteColor: String = "overview1",
+                     avatar: String = "pink",
+                     createdAt: Date? = nil) -> Self {
+        Contact(id: id,
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                jobtitle: jobtitle,
+                favouriteColor: favouriteColor,
+                avatar: avatar,
+                createdAt: createdAt)
+    }
+}
