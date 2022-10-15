4c254b97cc180bc8342864cfa586f751c7e9da06
Willy-SDK <>
2022-10-15 14:39:38 +0100

    -Implement Data Transfer Service Tests

--- ColleaguesContact/ColleaguesContactTests/Infrastructure/Network/DataTransferServiceTests.swift
+++ ColleaguesContact/ColleaguesContactTests/Infrastructure/Network/DataTransferServiceTests.swift
@@ -7,29 +7,126 @@
 
 import XCTest
 
-final class DataTransferServiceTests: XCTestCase {
+private struct MockModel: Decodable {
+    let name: String
+}
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class DataTransferServiceTests: XCTestCase {
+    
+    private enum DataTransferErrorMock: Error {
+        case someError
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_whenReceivedValidJsonInResponse_shouldDecodeResponseToDecodableObject() {
+        //given
+        let config = NetworkConfigurableMock()
+        let expectation = self.expectation(description: "Should decode mock object")
+        
+        let responseData = #"{"name": "Hello"}"#.data(using: .utf8)
+        let networkService = DefaultNetworkService(config: config, sessionManager: NetworkSessionManagerMock(response: nil,
+                                                                                                             data: responseData,
+                                                                                                             error: nil))
+        
+        let sut = DefaultDataTransferService(with: networkService)
+        //when
+        _ = sut.request(with: Endpoint<MockModel>(path: "http://mock.endpoint.com", method: .get)) { result in
+            do {
+                let object = try result.get()
+                XCTAssertEqual(object.name, "Hello")
+                expectation.fulfill()
+            } catch {
+                XCTFail("Failed decoding MockObject")
+            }
         }
+        //then
+        wait(for: [expectation], timeout: 0.1)
     }
-
+    
+    func test_whenInvalidResponse_shouldNotDecodeObject() {
+        //given
+        let config = NetworkConfigurableMock()
+        let expectation = self.expectation(description: "Should not decode mock object")
+        
+        let responseData = #"{"age": 20}"#.data(using: .utf8)
+        let networkService = DefaultNetworkService(config: config, sessionManager: NetworkSessionManagerMock(response: nil,
+                                                                                                             data: responseData,
+                                                                                                             error: nil))
+        
+        let sut = DefaultDataTransferService(with: networkService)
+        //when
+        _ = sut.request(with: Endpoint<MockModel>(path: "http://mock.endpoint.com", method: .get)) { result in
+            do {
+                _ = try result.get()
+                XCTFail("Should not happen")
+            } catch {
+                expectation.fulfill()
+            }
+        }
+        //then
+        wait(for: [expectation], timeout: 0.1)
+    }
+    
+    func test_whenBadRequestReceived_shouldRethrowNetworkError() {
+        //given
+        let config = NetworkConfigurableMock()
+        let expectation = self.expectation(description: "Should throw network error")
+        
+        let responseData = #"{"invalidStructure": "Nothing"}"#.data(using: .utf8)!
+        let response = HTTPURLResponse(url: URL(string: "test_url")!,
+                                       statusCode: 500,
+                                       httpVersion: "1.1",
+                                       headerFields: nil)
+        let networkService = DefaultNetworkService(config: config, sessionManager: NetworkSessionManagerMock(response: response,
+                                                                                                             data: responseData,
+                                                                                                             error: DataTransferErrorMock.someError))
+        
+        let sut = DefaultDataTransferService(with: networkService)
+        //when
+        _ = sut.request(with: Endpoint<MockModel>(path: "http://mock.endpoint.com", method: .get)) { result in
+            do {
+                _ = try result.get()
+                XCTFail("Should not happen")
+            } catch let error {
+                
+                if case DataTransferError.networkFailure(NetworkError.error(statusCode: 500, _)) = error {
+                    expectation.fulfill()
+                } else {
+                    XCTFail("Wrong error")
+                }
+            }
+        }
+        //then
+        wait(for: [expectation], timeout: 0.1)
+    }
+    
+    func test_whenNoDataReceived_shouldThrowNoDataError() {
+        //given
+        let config = NetworkConfigurableMock()
+        let expectation = self.expectation(description: "Should throw no data error")
+        
+        let response = HTTPURLResponse(url: URL(string: "test_url")!,
+                                       statusCode: 200,
+                                       httpVersion: "1.1",
+                                       headerFields: [:])
+        let networkService = DefaultNetworkService(config: config, sessionManager: NetworkSessionManagerMock(response: response,
+                                                                                                             data: nil,
+                                                                                                             error: nil))
+        
+        let sut = DefaultDataTransferService(with: networkService)
+        //when
+        _ = sut.request(with: Endpoint<MockModel>(path: "http://mock.endpoint.com", method: .get)) { result in
+            do {
+                _ = try result.get()
+                XCTFail("Should not happen")
+            } catch let error {
+                if case DataTransferError.noResponse = error {
+                    expectation.fulfill()
+                } else {
+                    XCTFail("Wrong error")
+                }
+            }
+        }
+        //then
+        wait(for: [expectation], timeout: 0.1)
+    }
 }
